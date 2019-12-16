9575d6748bd39097f3f50a4222342e55b33d1e7c
Taynan Souza <taynan.tws@gmail.com>
2019-12-16 18:10:03 -0300

    [SFS-44] (task) API to receive events of a message broker
    Google PubSub receive/subscribe method implemented.

--- type.go
+++ type.go
@@ -16,19 +16,3 @@ type UnprocessableEntityErrResponse struct {
 	ErrorResponse
 	InvalidAttributes interface{} `json:"invalid_attributes"`
 }
-
-// messageBroker provides an interface so we can swap out the
-// implementation of SendEmail under tests.
-type messageBroker interface {
-	send(topicID string, msg interface{}) error
-}
-
-// googlePubSub message broker
-type googlePubSub struct {
-	ProjectID string
-}
-
-// messageAPI standard message api
-type messageAPI struct {
-	API messageBroker
-}
